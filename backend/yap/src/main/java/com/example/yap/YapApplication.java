19099dd87f0ff41541a591480146941c1697dfa9
MahgoubHusien <mahgoubhhusien@gmail.com>
2025-03-09 17:30:45 -0500

    Save progress before rebasing

--- backend/yap/src/main/java/com/example/yap/YapApplication.java
+++ backend/yap/src/main/java/com/example/yap/YapApplication.java
@@ -1,19 +1,12 @@
 package com.example.yap;
 
-import com.example.yap.config.EnvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class YapApplication {
-
 	public static void main(String[] args) {
-		// Load environment variables from the .env file
-		EnvLoader.loadEnvFile("src/main/resources/.env");
-
-		// Optionally print variables for debugging
-		System.out.println("App Name: " + System.getProperty("APP_NAME"));
-
 		SpringApplication.run(YapApplication.class, args);
 	}
 }
+
