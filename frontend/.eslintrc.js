28094a9e894c37472a0131b428bcd22674580e02
MahgoubHusien <mahgoubhhusien@gmail.com>
2025-04-16 12:28:35 -0500

    Fix ESLint config location for Vercel

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,11 +1,10 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-    root: true,
-    extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      'react-hooks/exhaustive-deps': 'warn',
-    },
-  };
-  
+  root: true,
+  extends: ['next/core-web-vitals'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
